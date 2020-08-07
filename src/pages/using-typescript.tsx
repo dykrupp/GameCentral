9d5554bd8dc4d0fd7fb6d2ca30b3097f53ea5e09
Dylan Krupp <dylan.krupp@promessinc.com>
2020-08-07 17:10:47 -0400

    Linf fix

--- src/pages/using-typescript.tsx
+++ src/pages/using-typescript.tsx
@@ -1,7 +1,7 @@
 // If you don't want to use TypeScript you can delete this file!
 import React from 'react';
 import { PageProps, Link, graphql } from 'gatsby';
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 import SEO from '../components/seo';
 import PropTypes from 'prop-types';
 
