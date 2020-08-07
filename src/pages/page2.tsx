295a25838689292307f28d32cd21303feb0c68c8
dykrupp <dylankrupp@ymail.com>
2020-08-07 18:26:55 -0400

    -Fixed path

--- src/pages/page2.tsx
+++ src/pages/page2.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 import SEO from '../components/seo';
 
 const SecondPage: React.FC = () => (
@@ -8,6 +8,7 @@ const SecondPage: React.FC = () => (
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    <p>some stuff</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 );
