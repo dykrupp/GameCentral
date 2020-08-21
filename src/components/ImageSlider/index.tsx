a863bd9f8fa879e22ca3bb4f241b99f355c7f220
Dylan Krupp <dylan.krupp@promessinc.com>
2020-08-21 13:10:57 -0400

    -Added skeleton to ImageSlider when loading component

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
 import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
 import sliderStyles from './styles.module.css';
 import styled from 'styled-components';
 import { usePickedProductInfo } from '../../utils/hooks/usePickedProductInfo';
+import Skeleton from '@material-ui/lab/Skeleton';
+
+//TODO -> add button functionality w/ cursor change
 
 export const sliderHeight = '450px';
 
@@ -12,21 +15,32 @@ const ImageSliderContainer = styled.div`
   height: ${sliderHeight};
 `;
 
+const LoadingSkeleton = styled(Skeleton)`
+  height: ${sliderHeight};
+  transform: none;
+`;
+
 export const ImageSlider: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const pickedProductInfo = usePickedProductInfo();
 
-  if (!pickedProductInfo) return null;
   return (
-    <ImageSliderContainer>
-      <AwesomeSlider
-        animation="cubeAnimation"
-        bullets={true}
-        cssModule={sliderStyles}
-      >
-        {pickedProductInfo.map((productInfo) => (
-          <div key={productInfo.sku} data-src={productInfo.fluidObject.src} />
-        ))}
-      </AwesomeSlider>
-    </ImageSliderContainer>
+    <div>
+      {isLoading && <LoadingSkeleton animation="wave" />}
+      <ImageSliderContainer>
+        <AwesomeSlider
+          infinite={true}
+          animation="cubeAnimation"
+          bullets={true}
+          cssModule={sliderStyles}
+          style={{ display: isLoading ? 'none' : 'block' }}
+          onFirstMount={() => setIsLoading(false)}
+        >
+          {pickedProductInfo.map((productInfo) => (
+            <div key={productInfo.sku} data-src={productInfo.fluidObject.src} />
+          ))}
+        </AwesomeSlider>
+      </ImageSliderContainer>
+    </div>
   );
 };
