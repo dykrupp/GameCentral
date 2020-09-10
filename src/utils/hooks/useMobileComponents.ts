4af3cb5de2ab3f2dffc143e00ce142e266478b77
Dylan Krupp <dylan.krupp@promessinc.com>
2020-09-10 14:34:16 -0400

    -Reverted previous change

--- src/utils/hooks/useMobileComponents.ts
+++ src/utils/hooks/useMobileComponents.ts
@@ -1,4 +1,4 @@
 import { useMediaQuery } from '@material-ui/core';
 
 export const useMobileComponents = (): boolean =>
-  useMediaQuery('(max-width: 1500px)', { noSsr: true });
+  useMediaQuery('(max-width: 1500px)');
